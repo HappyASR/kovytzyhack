a85c912c74f6d6f9069bbddc380800d37ef9495c
kensou <100714928@qq.com>
2020-03-09 23:45:41 +0800

    Signed-off-by: kensou <100714928@qq.com>

--- build.bat
+++ build.bat
@@ -1,3 +1,13 @@
+mkdir build
 cd build
+del /q *.*
 cmake .. -DCMAKE_TOOLCHAIN_FILE=../pgm.cmake -G"Unix Makefiles"
-make -j8
+make
+pause
+cd ..
+libkov
+copy ytzy_v201cn_o.rom fbas\roms\ytzyws_prg.rom
+cd fbas\roms
+"C:\Program Files\WinRAR\winrar.exe" a -r kovytzyws.zip ytzyws_prg.rom
+cd ..
+fbas -g kovytzyws -w -r 480*224
